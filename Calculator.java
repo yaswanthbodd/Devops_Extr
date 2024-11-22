45064f9c11bb7452708dc2ebc15c3d1b8c082439
yash <124248317+yaswanthbodd@users.noreply.github.com>
2024-11-22 15:03:41 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+package com.devops;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=45;
+		int b=20;
+		char c='%';
+		int result=0;
+		switch(c) {
+		case '+' : result=a+b;
+				System.out.println("Addition of "+a+" + "+b+" : "+result);
+				break;
+		case '-' : result=a-b;
+				System.out.println("Subtraction of "+a+" - "+b+" : "+result);
+				break;
+		case '*' : result=a*b;
+				System.out.println("Multiplication of "+a+" * "+b+" : "+result);
+				break;
+		case '%' : result=a%b;
+				System.out.println("Modules of "+a+" % "+b+" : "+result);
+				break;
+		case '/' : result=a/b;
+				System.out.println("Division of "+a+" / "+b+" : "+result);
+				break;
+		}
+	}
+
+}
